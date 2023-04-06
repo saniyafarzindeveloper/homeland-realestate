722f65a57715e091a5d87aa79e33f72b8c4fee71
farzinsaniya <saniyafarzin75@gmail.com>
2023-04-06 14:47:49 +0530

    implementing bg of search

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,30 +1,33 @@
-import React from 'react';
-import Image from '../assets/img/house-banner.png'
-import Search from '../components/Search'
-
-
-
+import React from "react";
+import Image from "../assets/img/house-banner.png";
+import Search from "../components/Search";
 
 const Banner = () => {
   return (
-    <section className='h-full max-h-[640px] mb-8 xl-mb:24' >
-    <div className='flex flex-col lg:flex-row' >
-    <div className='lg:ml-8 xl:ml-[138px] flex flex-col items-center lg:items-start text-center lg:text-left justify-center flex-1 px-4 lg:px-0' >
-        <h1 className='text-4xl lg:text-[56px] font-semibold leading-none mb-5' >
-        <span className='' >Rent </span>Your Dream House With Us.
-        </h1>
-        <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium, vitae excepturi! Fugit quidem sed expedita ducimus eligendi quae esse autem sit temporibus architecto corrupti veniam, minima excepturi facilis, labore velit?</p>
-      </div>
+    <section className="h-full max-h-[640px] mb-8 xl-mb:24">
+      <div className="flex flex-col lg:flex-row">
+        <div className="lg:ml-8 xl:ml-[138px] flex flex-col items-center lg:items-start text-center lg:text-left justify-center flex-1 px-4 lg:px-0">
+          <h1 className="text-4xl lg:text-[56px] font-semibold leading-none mb-5">
+            <span className="text-violet-700">Rent </span>Your Dream House With Us.
+          </h1>
+          <p className="max-w-[480px] mb-8" >
+            {/* {" "} */}
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Laudantium, vitae excepturi! Fugit quidem sed expedita ducimus
+            eligendi quae esse autem sit temporibus architecto corrupti veniam,
+            minima excepturi facilis, labore velit?
+          </p>
+        </div>
 
-      {/* img */}
-      <div>
-        <img src={Image} alt='banner' />
+        {/* img */}
+        <div className="hidden flex-1 lg:flex justify-end items-end" >
+          <img src={Image} alt="banner" />
+        </div>
       </div>
-    </div>
-      
+ 
       <Search />
     </section>
-  )
+  );
 };
 
 export default Banner;
